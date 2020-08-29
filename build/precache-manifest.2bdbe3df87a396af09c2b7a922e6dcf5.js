5bae3b28f1710808bdfd05a8f97fc0d51ff42376
thProfessor <utkrs.sukla99@gmail.com>
2020-08-30 00:43:24 +0530

    2nd

---
+++ build/precache-manifest.2bdbe3df87a396af09c2b7a922e6dcf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b7f1c5d4ec84bdf89851fc91e5f396",
+    "url": "/letter/index.html"
+  },
+  {
+    "revision": "3ee3c3d19c6f8e113030",
+    "url": "/letter/static/css/main.c1faad12.chunk.css"
+  },
+  {
+    "revision": "6b3395afe1117a594841",
+    "url": "/letter/static/js/2.1b15b04f.chunk.js"
+  },
+  {
+    "revision": "1a7c6f7146f8b594252cb9ec08e8f0dd",
+    "url": "/letter/static/js/2.1b15b04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee3c3d19c6f8e113030",
+    "url": "/letter/static/js/main.bece7f4a.chunk.js"
+  },
+  {
+    "revision": "1c7f58c8f2db529a72dd",
+    "url": "/letter/static/js/runtime-main.164cfd15.js"
+  }
+]);
